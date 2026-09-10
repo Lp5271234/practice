c7e012345112121f1624ad7eda59d2e6bc21028e
Lp5271234 <3579405018@qq.com>
2026-09-10 22:59:06 +0800

    feat: add 9-10 C exercise

---
+++ c/tese-9-10/tese-9-10/tese.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+	printf("Hello, World!\n");
+	printf("'\n");
+	printf("%s", "C:\\Users\\lp\\Documents\\GitHub\\practice\n");
+	printf("\a");
+	printf("a\tbc\tdefgh\tyyyyy\t\n");
+	printf("abcdef\b\n");
+	printf("abcdefghijklmn\r\n");
+	printf("this is a today's notes i got a general understanding common escape characters");
+
+
+	return 0;
+}
