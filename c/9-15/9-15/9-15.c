ea5bdacf62c846d2d1f0bdf07d7d0149508c755c
Lp5271234 <3579405018@qq.com>
2026-09-15 09:33:30 +0800

    feat: add 9-15 C comments exercise

---
+++ c/9-15/9-15/9-15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	printf("hello world\n");
+	printf("hello world\n");
+	//这个是打印hello world的代码，了解了注释
+	/*一次可以注释多行，但不支持嵌套注释
+	c语言最早的嵌套方*/
+	//从双斜杠到行尾都是注释单行注释，可以放在行首，也可以放在行尾
+	//注释会被编译器替换成空格
+
+	return 0;
+}
