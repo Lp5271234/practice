739ee799f4d2d9c83a962c68eacee467e8240694
Lp5271234 <3579405018@qq.com>
2026-09-19 22:54:04 +0800

    feat: add 9-19 C arithmetic notes

---
+++ c/9-19/9-19/9-19.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main()
+{
+	int a = 10;
+	int b = 15;
+	printf("%d\n", a + b);
+	printf("%d\n", a * b);
+	printf("%d\n", a - b);
+	printf("%d\n", a / b);
+	printf("%d\n", a %b);
+	printf("%f\n", 10/15.0);
+	int age = 19;
+	printf("%d\n", age);
+	int lp = 19;
+	printf("%zu\n", sizeof(lp));
+	char *love = "i love you";
+	if ("love");
+	printf("%s", love);
+		return 0;
+}
